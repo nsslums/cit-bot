b471d4e624bed27d183cec820356f93645084a1c
pantyetta <email@pantyetta.com>
2023-04-17 16:38:30 +0900

    first commit | discord bot for python & java

---
+++ java/Cit-bot/src/main/java/Bot.java
@@ -0,0 +1,39 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import java.util.Collections;
+
+public class Bot extends ListenerAdapter {
+
+    public static void main(String[] args) {
+        JDA jda = JDABuilder.createLight("", Collections.emptyList())
+                .addEventListeners(new Bot())
+                .setActivity(Activity.watching("citサービス"))
+                .build();
+
+        jda.updateCommands().addCommands(
+                Commands.slash("ping", "description.")
+        ).queue();
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event)
+    {
+        // make sure we handle the right command
+        switch (event.getName()) {
+            case "ping":
+                long time = System.currentTimeMillis();
+                event.reply("Pong!").setEphemeral(true) // reply or acknowledge
+                        .flatMap(v ->
+                                event.getHook().editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time) // then edit original
+                        ).queue(); // Queue both reply and edit
+                break;
+        }
+    }
+}
